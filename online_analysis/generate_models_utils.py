98d82d7bb66f09e9b5b980eeb07d6cbe9f9dbb17
preeya <pkhanna104@gmail.com>
2020-05-07 11:36:31 -0700

    added plotting fcn to make sure extracted spike kinmeatics data looks right

--- online_analysis/generate_models_utils.py
+++ online_analysis/generate_models_utils.py
@@ -4,8 +4,11 @@ from resim_ppf import ppf_pa
 
 import numpy as np 
 import pandas
+import copy
 import pickle 
 import tables 
+
+import matplotlib.pyplot as plt
 
 pref = analysis_config.config['grom_pref']
 
@@ -31,11 +34,11 @@ def get_spike_kinematics(animal, day, order, history_bins, **kwargs):
     temp_tune['pos'] = []
     
     temp_tune['tsk'] = []
-    temp_tune['trg'] = [] ### Index ? 
+    temp_tune['trg'] = [] ### Index 
     temp_tune['trg_pos'] = [] ### position in space 
     temp_tune['trl'] = []
     temp_tune['psh'] = [] ### xy push 
-    temp_tune['trl_bin_ix'] = [] ### index from onset? 
+    temp_tune['trl_bin_ix'] = [] ### index from onset 
 
     ### Not sure what these are 
     trial_ord = {}
@@ -128,16 +131,40 @@ def get_spike_kinematics(animal, day, order, history_bins, **kwargs):
                 cursor_pos = [curs[:, [0, 2]] for curs in cursor_state]
                 cursor_vel = [curs[:, [3, 5]] for curs in cursor_state]
 
+                ####### TESTING ########
+                dTA = np.diff(targ_i_all, axis=0)
+                dTI = np.diff(targ_ix, axis=0)
+
+                ixx = np.nonzero(dTA[:, 0])[0]
+                ixy = np.nonzero(dTA[:, 1])[0]
+                ixxy = np.unique(np.hstack((ixx, ixy)))
+                ixt = np.nonzero(dTI)[0]
+                if len(ixxy) == len(ixt):
+                    assert(np.all(ixxy == ixt))
+                elif len(ixxy) < len(ixt):
+                    assert(np.all(i in ixt for i in ixxy))
+                else:
+                    raise Exception('Shouldnt be more target changes than trial changes')
+
+                ### Sometimes targets can be repeated so just 
+                ### make sure all ixxys are in ixtrl
+                ixtrl = np.nonzero(np.diff(trial_ix_all))[0]
+                assert(np.all(i in ixtrl for i in ixxy))
+
                 tmp = np.nonzero(targ_ix >= 0)[0]
                 if i_t == 1:
                     ### 9 obstacle target ###
-                    assert(len(np.unique(targ_ix[tmp])) == 9)
+                    assert(np.max(np.unique(targ_ix[tmp])) > 8)
+                    print('Len Obs Targs %s'%str(np.unique(targ_ix[tmp])))
                 else:
                     assert(len(np.unique(targ_ix[tmp])) == 8)
 
-                trl_rd.append(order[i_t][i])
-                import pdb; pdb.set_trace()
+                ###################################
+                ######## End of TESTING ###########
+                ###################################
 
+                trl_rd.append(order[i_t][i])
+                
                 # f, ax = plt.subplots(ncols=3, nrows=3)
                 # if i_t == 0:
                 #     fall, axll = plt.subplots()
@@ -182,13 +209,18 @@ def get_spike_kinematics(animal, day, order, history_bins, **kwargs):
                 trg_trl_pos.append(targ_i_all[ix_x, :])
 
             ### Remove trials that are -1: 
+            ### Keep these trials ####
             ix_mod = np.array([iii for iii in ix_mod if iii not in rm_trls])
 
             # Add to list: 
+            ### Stack up the binned spikes; 
             spks.append(np.vstack(([bin_spk[x] for x in ix_mod])))
             
             # We need the real velocity: 
             vel.append(np.vstack(([cursor_vel[x] for x in ix_mod])))
+
+            # Get the push too? 
+            push.append(np.vstack(([decoder_all[x] for x in ix_mod])))
 
             # Not the Kalman Gain: 
             tsk.append(np.zeros(( np.vstack(([bin_spk[x] for x in ix_mod])).shape[0], )) + i_t)
@@ -222,21 +254,24 @@ def get_spike_kinematics(animal, day, order, history_bins, **kwargs):
                     ix_[:, 0] = t + tsk_trl_cnt
                     ix_[:, 1] = order[i_t][i] # Order
 
-                    ### For each time bin that we can actually capture: 
-                    for tm in np.arange(history_bins, nt-history_bins):
-                        
-                        for tmpi, tmpii in enumerate(np.arange(tm - history_bins, tm + history_bins + 1)):
-                            spk[tm-history_bins, tmpi, :] = trl[tmpii, :]
+                    XY_div = (2*history_bins)+1
 
-                        spk0[tm-history_bins, :] = trl[tm, :]
-                        velo[tm-history_bins, :(2*history_bins)+1] = np.squeeze(np.array(cursor_vel[t][tm-history_bins:tm+history_bins+1, 0]))
-                        velo[tm-history_bins, ((2*history_bins)+1):] = np.squeeze(np.array(cursor_vel[t][tm-history_bins:tm+history_bins+1, 1]))
+                    ### For each time bin that we can actually capture: 
+                    for tmi, tm in enumerate(np.arange(history_bins, nt-history_bins)):
                         
-                        poso[tm-history_bins, :(2*history_bins)+1] = np.squeeze(np.array(cursor_pos[t][tm-history_bins:tm+history_bins+1, 0]))
-                        poso[tm-history_bins, (2*history_bins)+1:] = np.squeeze(np.array(cursor_pos[t][tm-history_bins:tm+history_bins+1, 1]))
+                        ### for the individual time bins; 
+                        for tmpi, tmpii in enumerate(np.arange(tm - history_bins, tm + history_bins + 1)):
+                            spk[tmi, tmpi, :] = trl[tmpii, :]
+
+                        spk0[tmi, :] = trl[tm, :]
+                        velo[tmi, :XY_div] = np.squeeze(np.array(cursor_vel[t][tm-history_bins:tm+history_bins+1, 0]))
+                        velo[tmi, XY_div:] = np.squeeze(np.array(cursor_vel[t][tm-history_bins:tm+history_bins+1, 1]))
                         
-                        pusho[tm-history_bins, :(2*history_bins)+1] = np.squeeze(np.array(decoder_all[t][tm-history_bins:tm+history_bins+1, 3]))
-                        pusho[tm-history_bins, (2*history_bins)+1:] = np.squeeze(np.array(decoder_all[t][tm-history_bins:tm+history_bins+1, 5]))
+                        poso[tmi, :XY_div] = np.squeeze(np.array(cursor_pos[t][tm-history_bins:tm+history_bins+1, 0]))
+                        poso[tmi, XY_div:] = np.squeeze(np.array(cursor_pos[t][tm-history_bins:tm+history_bins+1, 1]))
+                        
+                        pusho[tmi, :XY_div] = np.squeeze(np.array(decoder_all[t][tm-history_bins:tm+history_bins+1, 3]))
+                        pusho[tmi, XY_div:] = np.squeeze(np.array(decoder_all[t][tm-history_bins:tm+history_bins+1, 5]))
                     
                         
                     temp_tune['spk0'].append(spk0)
@@ -251,32 +286,21 @@ def get_spike_kinematics(animal, day, order, history_bins, **kwargs):
                     temp_tune['trl_bin_ix'].append(np.arange(history_bins, nt-history_bins))
 
             tsk_trl_cnt += len(bin_spk)
-    # Get trial Order: Temporally order targets: 
-    #    temp_trial_order = np.vstack((temp_tune['trl']))
-
-    # n0 = 0
-    # for jj in range(len(trial_ord.keys())):
-    #     mx = np.max(trial_ord[jj])+1
-    #     trial_ord[jj] = trial_ord[jj]+n0
-    #     ixx = np.nonzero(temp_trial_order[:, 1]==jj)[0]
-    #     temp_trial_order[ixx, 0] += n0
-    #     n0 += mx
-
-    # Add them to trial_order in the right order: 
-    # trial_order = []
-    # for jj in range(len(trial_ord.keys())):
-    #     trial_order.append(trial_ord[trl_rd[jj]])
-
-    # trial_order = np.hstack((trial_order))
-
+    
+    ################################################
+    #### STACK UP EVERYTHING WITHOUT TIME SHIFTS ###
+    ################################################
     spikes = np.vstack((spks))
-
     velocity = np.vstack((vel))
+
     if velocity.shape[1] == 7:
         velocity = np.array(velocity[:, [3, 5]])
+    
     position = np.vstack((pos))
     task_index = np.hstack((tsk))
-    
+    push = np.vstack((push))
+
+
     #################################
     ######## Model Options ##########
     #################################
@@ -285,46 +309,53 @@ def get_spike_kinematics(animal, day, order, history_bins, **kwargs):
     sub_spk0_temp_all = np.vstack((temp_tune['spk0']))
     nneur = sub_spk_temp_all.shape[2]
 
+    #### Time lags ####
     TL = np.arange(-1*history_bins, history_bins+1)
     i_zero = np.nonzero(TL==0)[0]
-    hb2 = (2*history_bins) + 1; 
 
-    sub_push_all = np.vstack((temp_tune['psh']))[:, [i_zero, i_zero+hb2]]
+    #### Push at time zero and time zero + future ####
+    #### Current push X / Y 
+    sub_push_all = np.vstack((temp_tune['psh']))[:, [i_zero, i_zero+XY_div]]
+
+    #### Get all the temporally shifted data ###
     psh_temp = np.vstack((temp_tune['psh']))
     vel_temp = np.vstack((temp_tune['vel']))
     tsk_temp = np.hstack((temp_tune['tsk']))
     pos_temp = np.vstack((temp_tune['pos']))
     trg_temp = np.hstack((temp_tune['trg']))
 
+    #### Target positions / trial bin indices / trial number 
     trg_pos_temp = np.vstack((temp_tune['trg_pos']))
     bin_temp = np.hstack((temp_tune['trl_bin_ix']))
     trl_temp = np.vstack((temp_tune['trl']))[:, 0]
-    #trl_temp = temp_trial_order[:, 0]
 
+    ### Generate acceleration #####
     accx = np.hstack((np.array([0.]), np.diff(velocity[:, 0])))
     accy = np.hstack((np.array([0.]), np.diff(velocity[:, 1])))
 
-    ###################################
-    ### MODEL with t, VEL, POS, ACC ###
-    ###################################
+    ########################################################################
+    ### Add all the data to the pandas frame with t, VEL, POS, ACC ###
+    #######################################################################
 
-    data = pandas.DataFrame({'velx': velocity[:, 0], 'vely': velocity[:, 1],'tsk': task_index, 'posx': position[:, 0], 'posy': position[:, 1]})
+    data = pandas.DataFrame({'velx': velocity[:, 0], 'vely': velocity[:, 1],'tsk': task_index, 
+        'posx': position[:, 0], 'posy': position[:, 1]})
+
+    ### Add it to the end 
     data['accx'] = pandas.Series(accx, index=data.index)
     data['accy'] = pandas.Series(accy, index=data.index)
 
+    ### Time lags 
     TL = np.arange(-1*history_bins, history_bins+1)
-    tli = TL - TL[0]
-    hb2 = (2*history_bins)+1
 
     tmp_dict = {}
-    for i in tli:
-        if TL[i] <= 0:
+    for i, tli in enumerate(TL):
+        if tli <= 0:
             nm0 = 'm'
-        elif TL[i] > 0:
+        elif tli > 0:
             nm0 = 'p'
 
         for vl in ['vel', 'pos', 'psh']:
-            for ixy, (nm, xy_ad) in enumerate(zip(['x', 'y'], [0, hb2])):
+            for ixy, (nm, xy_ad) in enumerate(zip(['x', 'y'], [0, XY_div])):
                 if vl=='vel':
                     tmp = vel_temp[:, i + xy_ad]
                 elif vl == 'pos':
@@ -332,7 +363,7 @@ def get_spike_kinematics(animal, day, order, history_bins, **kwargs):
                 elif vl == 'psh':
                     tmp = psh_temp[:, i + xy_ad]
 
-                tmp_dict[vl+nm+'_t'+nm0+str(np.abs(TL[i]))] = tmp
+                tmp_dict[vl+nm+'_t'+nm0+str(np.abs(TL[i]))] = copy.deepcopy(tmp)
 
         for n in range(nneur):
             tmp_dict['spk_t'+nm0+str(np.abs(TL[i]))+'_n'+str(n)] = sub_spk_temp_all[:, i, n]
@@ -344,10 +375,80 @@ def get_spike_kinematics(animal, day, order, history_bins, **kwargs):
     tmp_dict['trg_posy'] = trg_pos_temp[:, 1]
     tmp_dict['bin_num'] = bin_temp
     tmp_dict['trl'] = trl_temp; 
+    
     ##################################
     ### MODEL with HISTORY +FUTURE ###
     ##################################
     data_temp = pandas.DataFrame(tmp_dict)
-    plot_data_temp(data_temp, True)
+    plot_data_temp(data_temp, animal, True)
 
     return data, data_temp, sub_spk0_temp_all, sub_spk_temp_all, sub_push_all
+
+
+### Confirmation that extracted data looks right ###
+def plot_data_temp(data_temp, animal, use_bg = False):
+    
+    ### COLORS ####
+    cmap_list = analysis_config.pref_colors
+    co_obs_cmap = [np.array([0, 103, 56])/255., np.array([46, 48, 146])/255., ]
+
+    if animal == 'jeev':
+        use_bg = False
+
+    ### for each target new plto: 
+    fco, axco = plt.subplots()
+    fob, axob = plt.subplots()
+    
+    for ax in [axco, axob]:
+        ax.axis('square')
+    tgs = [4, 5]
+
+    ### open target: 
+    for i in range(2): 
+        tsk_ix = np.nonzero(data_temp['tsk'] == i)[0]
+        targs = np.unique(data_temp['trg'][tsk_ix])
+
+        for itr, tr in enumerate(targs):
+            ## Get the trial numbers: 
+            targ_ix = np.nonzero(data_temp['trg'][tsk_ix] == tr)[0]
+            trls = np.unique(data_temp['trl'][tsk_ix[targ_ix]])
+
+            if tgs[i] == itr:
+                alpha = 1.0; LW = 2.0
+            else:
+                alpha = 0.4; LW = 1.0
+
+            if i == 0:
+                axi = axco; 
+            else:
+                axi = axob#[itr/3, itr%3]
+
+            for trl in trls:
+                ix = np.nonzero(data_temp['trl'][tsk_ix] == trl)[0]
+                if use_bg:
+                    axi.plot(data_temp['posx_tm0'][tsk_ix[ix]], data_temp['posy_tm0'][tsk_ix[ix]], '-', color=co_obs_cmap[i], linewidth = LW, alpha=alpha)
+                else:
+                    axi.plot(data_temp['posx_tm0'][tsk_ix[ix]], data_temp['posy_tm0'][tsk_ix[ix]], '-', color=cmap_list[itr])
+
+    ### Add target info for Grom: 
+    for i, a in enumerate(np.linspace(0., 2*np.pi, 9)):
+        if i < 8:
+            tg = [10*np.cos(a), 10*np.sin(a)]
+            circle = plt.Circle(tg, radius=1.7, color = cmap_list[i], alpha=.2)
+            axob.add_artist(circle)
+            circle = plt.Circle(tg, radius=1.7, color = cmap_list[i], alpha=.2)
+            axco.add_artist(circle)
+
+        for ax in [axco, axob]:
+            if animal == 'grom':
+                ax.set_xlim([-12, 12])
+                ax.set_ylim([-12, 12])
+                ax.set_xticks([])
+                ax.set_yticks([])
+            else:
+                ax.set_xlim([-.5, 2.5])
+                ax.set_ylim([1.2, 5.0])
+                ax.set_xticks([])
+                ax.set_yticks([])
+    import pdb; pdb.set_trace()
+
