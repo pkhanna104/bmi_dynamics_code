c39d421bc8677797a96d197d45f8839af9942fba
preeya <pkhanna104@gmail.com>
2020-10-07 16:50:37 -0700

    added fcn to plot obstacles for monkey j

--- resim_ppf/ppf_pa.py
+++ resim_ppf/ppf_pa.py
@@ -5,6 +5,8 @@ import file_key as fk
 import pickle
 import os
 
+from online_analysis import util_fcns
+
 def get_jeev_trials(filename, binsize=.1):
     try:
         dat = sio.loadmat(filename)
@@ -252,7 +254,8 @@ def _bin_spike_counts(ixs, spike_counts, spk_counts_dt, binsize, pre_go):
     return bin_spk, bin_spk_ub, exclude
 
 def _bin_cursor_kin(ixs, cursor_kin, binsize, pre_go = 0.):
-    n_per_bin = 20
+    n_per_bin = int(binsize/.005)
+    print('Bin Curson kin Size %d' %(n_per_bin))
     pre_go_bins = int( pre_go / .005 ); 
 
     bin_ck = []
@@ -322,6 +325,25 @@ def _bin_ad(times, dat, binsize):
         bin_ad.append(X)
     return bin_ad
 
+def get_targ_ix(strobed, go_ix, task):
+    if task == 'co':
+        targ = strobed[go_ix - 2, 1]
+        targ_ix = targ - 64
+        assert np.sum(fk.cotrialList[targ_ix] - targ) == 0
+
+    elif task == 'obs':
+        targ = [strobed[g-4:g-1, 1] for i, g in enumerate(go_ix)]
+        targ_ix = []
+        for i, tg in enumerate(targ):
+            tmp = np.tile(tg[np.newaxis, :], [len(fk.obstrialList), 1])
+            ix = np.nonzero(np.sum(np.abs(fk.obstrialList - tmp), 1) == 0)[0]
+            if len(ix)==1:
+                targ_ix.append(ix[0])
+            else:
+                targ_ix.append(-1)
+        targ_ix = np.array(targ_ix)
+    return targ_ix
+
 def make_strobed(dat, start_index_overall):
     strobed = []
     events = dat['task_events']
@@ -336,23 +358,24 @@ def make_strobed(dat, start_index_overall):
                     strobed.append([i, ee[0]])
     return np.array(strobed)
 
-def plot_jeev_trials(task = 'obs'):
+def plot_jeev_trials(task = 'obs', targ_only = 3, day_ix = 0):
     input_type = fk.task_filelist
     
     if task == 'co':
-        te_num = input_type[0][0][0]
+        te_num = input_type[day_ix][0][0]
+    
     elif task == 'obs':
-        te_num = input_type[0][1][0]
+        te_num = input_type[day_ix][1][0]
     
     bin_spk, targ_i_all, targ_ix, trial_ix_all, decoder_all, cursor_state, unbinned, exclude = get_jeev_trials_from_task_data(te_num,
-        include_pos=True, binsize=.1)
+        include_pos=True, binsize=.005)
 
     colors = ['maroon', 'orangered', 'goldenrod','olivedrab','teal', 'steelblue', 'midnightblue', 'darkmagenta', 'k', 'brown']
 
     f, ax = plt.subplots()
 
     targs = np.unique(targ_ix)
-    targs = targs[targs >= 0]
+    targs = targs[targs == targ_only]
 
     for i in targs:
 
@@ -364,24 +387,300 @@ def plot_jeev_trials(task = 'obs'):
 
         for trl in trl_ix:
             ax.plot(cursor_state[trl][:, 0], cursor_state[trl][:, 2], '-', color = colors[i], linewidth=1.0)
+            ax.plot(cursor_state[trl][0, 0], cursor_state[trl][0, 2], 'r.')
 
         ### Plot the target: 
         if task == 'co':
             ax.plot(np.array([.04, targ_i_all[ix[0], 0]])*20, np.array([.14, targ_i_all[ix[0], 1]])*20, 'k-.')
+            ### For binsize == 0.005; 
+            #ax.plot(np.array([.04, targ_i_all[ix[0], 0]])*1, np.array([.14, targ_i_all[ix[0], 1]])*1, 'k-.')
         else:
-            tg = targ_i_all[ix[0], :]
+            #tg = targ_i_all[ix[0], :]
 
             ### Subtract obstacle target center; 
-            tg = tg - np.array([0., 2.5])
+            #tg = tg - np.array([0., 2.5])
 
             ### Convert cm --> m 
-            tg = tg / 100.
+            #tg = tg / 100.
 
             ### Add back the other center; 
-            tg = tg + np.array([.04, .14])
+            #tg = tg + np.array([.04, .14])
+            
+            #### Test obstacles ####
+            x = sio.loadmat('resim_ppf/jeev_obs_positions_from_amy.mat')
+            targ_list = fk.obstrialList
+            targ_series = targ_list[i, :] - 63
+            TC0 = x['targObjects'][:, :, int(targ_series[0])-1]
+            TC1 = x['targObjects'][:, :, int(targ_series[1])-1]
+            TC2 = x['targObjects'][:, :, int(targ_series[2])-1]
 
+            for t in [TC0, TC1, TC2]:
+
+                ### Subtract center; 
+                t_dem = t - np.array([0., 2.5])[:, np.newaxis]
+
+                ### Convert from cm --> m 
+                t_dem_m = t_dem / 100.
+
+                ### Add other center; 
+                centerPos = np.array([ 0.0377292,  0.1383867])
+                t_ = t_dem_m + centerPos[:, np.newaxis]
+                ax.plot(t_[0, :], t_[1, :], 'k-')
+                
             ### Plot the whole thing
-            ax.plot(np.array([.04, tg[0]])*20, np.array([.14,tg[1]])*20, 'k-.')
+            #ax.plot(np.array([.04, tg[0]])*20, np.array([.14,tg[1]])*20, 'k-.')
+            ### For binsize == 0.005; 
+            #ax.plot(np.array([.04, tg[0]])*1, np.array([.14,tg[1]])*1, 'k-.')
+
+def plot_percent_correct_t2t(plot=True, min_obs_targ = 2): 
+    input_type = fk.task_filelist
+    
+    metrics = dict()
+    metrics['co_pc'] = []
+    metrics['obs_pc'] = []
+
+    metrics['co_tt'] = []
+    metrics['co_tt_mn'] = []
+
+    metrics['obs_tt'] = []
+    metrics['obs_tt_mn'] = []   
+
+    metrics['perc_fulfill_obs'] = []
+    metrics['perc_fulfill_obs_mn'] = []
+     
+    
+    tsk_keys = ['co', 'obs']
+
+    f, ax = plt.subplots(figsize = (3, 4))
+    f2, ax2 = plt.subplots(figsize = (3, 4))
+    f3, ax3 = plt.subplots(figsize = (3, 4))
+
+    for i_d, day in enumerate(input_type):
+        day_perc_fulfill = []; 
+
+        for i_tsk, tsk in enumerate(day):
+            for i_te, filename in enumerate(tsk):
+
+                tsk_key = tsk_keys[i_tsk]
+
+                ####### Get percent correct #############
+                if 'jeev082413_VFB_PPF_B100_NS5_NU20_Z1_assist_ofc_cont_cont_assist_ofc_fixData' in filename:
+                    start_index_overall = 55003
+                else:
+                    start_index_overall = 0
+
+                ####### Load the correct file #######
+                filelist_task = []
+                for i, j in enumerate(fk.task_filelist):
+                    for k, l in enumerate(j):
+                        filelist_task.extend(l)
+                try:
+                    dat = sio.loadmat(filename)
+                except:
+                    ix = [j for i, j in enumerate(filelist_task) if filename in j]
+                    assert(len(ix)==1)
+                    
+                    try:
+                        dat = sio.loadmat(fk.task_directory+ix[0])
+                    except:
+                        dat = sio.loadmat(fk.task_directory_mbp+ix[0])
+                
+                ###### Get strobed #########
+                strobed = make_strobed(dat, start_index_overall)
+                cursor_kin = dat['cursor_kin']
+
+                go_ix = np.nonzero(strobed[:, 1] == 5)[0]
+                print('1. Total Go Ix: %d' %(len(go_ix)))
+
+                ###### Successfully get out of the center #####
+                keep_ix = np.nonzero(strobed[go_ix+1, 1] == 6)[0]
+                go_ix = go_ix[keep_ix]
+                go_ix = go_ix[go_ix < len(strobed) - 3]
+                print('2. Go Ix, out of center: %d' %(len(go_ix)))
+                
+                ##### Remove targets 0, 1 for obstacle; 
+                ### Get target index: 
+                targ_ix = get_targ_ix(strobed, go_ix, tsk_key)
+
+                if tsk_key == 'obs':
+                    keep_ix = np.nonzero(targ_ix >= min_obs_targ)[0]
+                else:
+                    keep_ix = np.arange(len(targ_ix))
+
+                go_ix = go_ix[keep_ix]
+                print('3. Go Ix, rm targs obs only: %d' %(len(go_ix)))
+                
+                ##### Obstacle collision = 300 ########
+                obs_coll_ix = np.nonzero(strobed[go_ix+2, 1] == 300)[0]
+
+                ##### Target timeout = 12 ##########
+                timeout_ix = np.nonzero(strobed[go_ix+2, 1] == 12)[0]
+
+                ###### Target hold error = 8 #######
+                the_ix = np.nonzero(strobed[go_ix + 3, 1] == 8)[0]
+
+                ##### Rew Ix 
+                rew_ix = np.nonzero(strobed[go_ix + 3, 1] == 9)[0]
+
+                #### Are all trials accounted for ####? 
+                ### Make sure no overlap 
+                assert len(np.unique(np.hstack((rew_ix, the_ix, timeout_ix, obs_coll_ix)))) == len(np.hstack((rew_ix, the_ix, timeout_ix, obs_coll_ix)))
+                
+                ### Make sure all trials accounted for ###
+                if len(np.unique(np.hstack((rew_ix, the_ix, timeout_ix, obs_coll_ix)))) == len(go_ix):
+                    N_trls = len(go_ix)
+                else:
+                    ### These notes for for go_ix with all obs targets; May be different if ignore obs target 0, 1; 
+                    ### this happened once in day 0 --> 5, 6, 15, 4, go_ix[205]
+                    ### with 25 sec in between 6 --> 15 
+                    ### Maybe kinarm task restarted? 
+
+                    ### happened once day 1 --> go_ix[185], seems like skipped 9? 
+
+                    ### Day 2 --> [5, 6, 9] at go_ix[132], seems like skipped 7; 
+                    print('Day %d, Task %d, Discrepancy %d' %(i_d, i_tsk, len(go_ix) - len(np.hstack((rew_ix, the_ix, timeout_ix, obs_coll_ix)))))
+                    N_trls = len(np.unique(np.hstack((rew_ix, the_ix, timeout_ix, obs_coll_ix))))
+                    
+                #### Add percent correct 
+                metrics[tsk_key + '_pc'].append(float(len(rew_ix)) / float(N_trls))
+
+                #### Trial time #####
+                trl_time = strobed[go_ix[rew_ix] + 3, 0] - strobed[go_ix[rew_ix], 0]
+                metrics[tsk_key + '_tt'].append(trl_time*.005)
+                metrics[tsk_key + '_tt_mn'].append(np.mean(trl_time*.005))
+
+                ##### For each rewarded trial, get the 3 targets: 
+                if i_tsk == 0: 
+
+                    for i_trl, trl_go in enumerate(go_ix[rew_ix]): 
+                        targ_ = strobed[trl_go - 2, 1]
+                        go_ = strobed[trl_go, 0]
+                        rew_ = strobed[trl_go + 3, 0]
+
+                        ### Get cursor trajectories ###
+                        trl_cursor = cursor_kin[[0, 1], go_:rew_] - fk.centerPos[:, np.newaxis]
+
+                        ### Which target are we in ####
+                        targ_ix = np.nonzero(fk.cotrialList == targ_)[0]
+                        tp = fk.targetPos[targ_ix, :] - fk.centerPos; 
+                        tp_norm = tp / np.linalg.norm(tp)
+
+                        ### Rotate the cursor #### 
+                        angle = np.arctan2(tp_norm[0, 1], tp_norm[0, 0])
+                        Rot = np.array([[np.cos(-1*angle), -np.sin(-1*angle)], [np.sin(-1*angle), np.cos(-1*angle)]])
+    
+                        ### Does the path go through any of the obstacle targets? How many? 
+                        trl_cursor_rot = np.dot(Rot, trl_cursor)
+                        perc_obs_viol = test_perc_obs_viol(trl_cursor_rot, min_obs_targ, plot=plot)
+                        
+                        metrics['perc_fulfill_obs'].append(1. - perc_obs_viol)
+                        day_perc_fulfill.append(1. - perc_obs_viol)
+
+                        if plot:
+                            import pdb; pdb.set_trace()
+
+        day_perc_fulfill = np.hstack((day_perc_fulfill))
+        metrics['perc_fulfill_obs_mn'].append(np.mean(day_perc_fulfill))
+
+    ##### plot these guys #####
+    ########### PERCENT CORRECT ##############
+    util_fcns.draw_plot(0, np.hstack((metrics['co_pc'])), 'g', [1., 1., 1., 0.], ax, width = .5)
+    util_fcns.draw_plot(1, np.hstack((metrics['obs_pc'])),'b', [1., 1., 1., 0.], ax, width = .5)
+    print('mean perc correct CO %.2f, OBS %.2f' %(np.mean(np.hstack((metrics['co_pc']))), 
+        np.mean(np.hstack((metrics['obs_pc'])))))
+
+    for _, (c, o) in enumerate(zip(metrics['co_pc'], metrics['obs_pc'])):
+        ax.plot([0, 1], [c, o], '-', color='gray', linewidth=0.5)
+    ax.set_xlim([-0.8, 1.8])
+    ax.set_ylim([0., 1.])
+    ax.set_ylabel('Percent Correct')
+    ax.set_xticks([0, 1])
+    ax.set_xticklabels(['CO', 'OBS'], rotation=45)
+
+    ########### Target TIME ##############
+    util_fcns.draw_plot(0, np.hstack((metrics['co_tt'])), 'g', [1., 1., 1., 0.], ax2, width = .5)
+    util_fcns.draw_plot(1, np.hstack((metrics['obs_tt'])),'b', [1., 1., 1., 0.], ax2, width = .5)
+    for _, (c, o) in enumerate(zip(metrics['co_tt_mn'], metrics['obs_tt_mn'])):
+        ax2.plot([0, 1], [c, o], '-', color='gray', linewidth=0.5)
+    ax2.set_xlim([-0.8, 1.8])
+    ax2.set_ylim([0., 7.5])
+    ax2.set_ylabel('Time to Target (sec)')
+    ax2.set_xticks([0, 1])
+    ax2.set_xticklabels(['CO', 'OBS'], rotation=45)
+
+    ########### Target TIME ##############
+    util_fcns.draw_plot(0, np.hstack((metrics['perc_fulfill_obs'])), 'g', [1., 1., 1., 0.], ax3, width = .5)
+    for _, o in enumerate(metrics['perc_fulfill_obs_mn']):
+        ax3.plot(0, o, '.', color='gray')
+    ax3.set_xlim([-0.8, 0.8])
+    ax3.set_ylim([0., 1.0])
+    ax3.set_ylabel('% Fulfill Obs.')
+    ax3.set_xticks([0])
+    ax3.set_xticklabels(['CO'], rotation=45)
+
+def test_perc_obs_viol(trl_curs, min_obs_targ, plot=False): 
+
+    ### Rotate and stretch all the targets to line up to 
+    x = sio.loadmat('resim_ppf/jeev_obs_positions_from_amy.mat')
+    targ_list = fk.obstrialList
+    targ_list = targ_list[min_obs_targ:, :]
+    rad = 0.065
+
+    if plot:
+        f, ax = plt.subplots()
+        ax.plot(trl_curs[0, :], trl_curs[1, :], 'k-')
+        ax.plot(trl_curs[0, 0], trl_curs[1, 0], 'r.')
+
+    violate = np.zeros((targ_list.shape[0]))
+
+    for i in range(targ_list.shape[0]):
+        targ_series = targ_list[i, :] - 63
+        TC0 = np.mean(x['targObjects'][:, :, int(targ_series[0])-1], axis=1)
+        TC2 = np.mean(x['targObjects'][:, :, int(targ_series[2])-1], axis=1) - TC0
+        
+        ### Center by TC0
+        ### Rotate by TC2; 
+        ### What is the angle with respect to center point; 
+        TC2_norm = TC2 / np.linalg.norm(TC2)
+        angle = np.arctan2(TC2_norm[1], TC2_norm[0])
+        R = np.array([[np.cos(-1*angle), -np.sin(-1*angle)], [np.sin(-1*angle), np.cos(-1*angle)]])
+        
+        ### Scaling on x-axis ###
+        TC2_rot = np.dot(R, TC2[:, np.newaxis])
+        scale_all = rad/TC2_rot[0]
+        
+        ### Now get the obstacle target; 
+        targ = targ_series[1]
+        TC1 = x['targObjects'][:, :, int(targ)-1] # 2 x 100 
+        T_dem = TC1-TC0[:, np.newaxis]
+        T_rot = np.dot(R, T_dem)
+        T_rot_scale = T_rot*scale_all; 
+
+        if plot: 
+            #### Plot obstacle ###
+            ax.plot(T_rot_scale[0, :], T_rot_scale[1, :], 'b-')
+
+        ### Does the cursor pass through this?
+        xrad = 0.5*(np.max(T_rot_scale[0, :]) - np.min(T_rot_scale[0, :]))
+        yrad = 0.5*(np.max(T_rot_scale[1, :]) - np.min(T_rot_scale[1, :]))
+
+        ### Get the center point; 
+        xmn = np.mean(T_rot_scale[0, :])
+        ymn = np.mean(T_rot_scale[1, :])
+
+        val = ((trl_curs[0, :] - xmn)**2 / (xrad**2)) + ((trl_curs[1, :] - ymn)**2 / (yrad**2))
+        if np.any(val <= 1): 
+            ix = np.nonzero(val <= 1)[0]
+            violate[i] = 1; 
+
+            if plot: 
+                ax.plot(trl_curs[0, ix], trl_curs[1, ix], 'm.')
+
+    return np.sum(violate) / float(len(violate))
+
+
+
+            
 
 
 
